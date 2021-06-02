a021ff2c7ac806d75160b4eaf8281ffe50c9cd38
Daniel Wzoreck <wzoreck2000@gmail.com>
2021-06-02 09:25:59 -0300

    Exemplo de Doctests adicionado

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,31 @@
-# Utilizando Assertions (voltado para outros desenvolvedores)
-
 def soma(x, y):
-    # Verificando se x é uma instância de int ou float
+    # Utilizando Assertions (voltado para outros desenvolvedores)
+    # Verificando se x é uma instância de int ou float (Assertions)
     assert isinstance(x, (int, float)), 'X precisa ser do tipo int ou float'
     return x + y
+
+
+def soma2(x, y):
+    # Doctests (Escrever testes na documentação diretamente) - precisa executar este arquivo!
+    # (chama a função e verifica se o retorno condiz com o resultado esperado)
+    """Soma x e y
+
+    >>> soma2(10, 20)
+    30
+
+    >>> soma2(5, 2)
+    90
+
+    >>> soma2('10', 30)
+    Traceback (most recent call last):
+    ...
+    AssertionError: X não é int ou float
+    """
+    # Este último teste passará pois foi feito tratamento de excessão
+    assert isinstance(x, (int, float)), 'X não é int ou float'
+    return x + y
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(verbose=True)
