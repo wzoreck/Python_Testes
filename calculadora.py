b07d2b5c9e374d1726583db99ee2751c60e507f0
Daniel Wzoreck <wzoreck2000@gmail.com>
2021-06-02 08:36:06 -0300

    Utilizando Assertions

--- calculadora.py
+++ calculadora.py
@@ -1,2 +1,6 @@
+# Utilizando Assertions (voltado para outros desenvolvedores)
+
 def soma(x, y):
+    # Verificando se x é uma instância de int ou float
+    assert isinstance(x, (int, float)), 'X precisa ser do tipo int ou float'
     return x + y
--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from calculadora import soma
 
-print(f'A soma entre 10 e 20 é: {soma(10, 20)}')
-print(f'A soma entre -10 e 20 é: {soma(-10, 20)}')
+# print(f'A soma entre 10 e 20 é: {soma(10, 20)}')
+# print(f'A soma entre -10 e 20 é: {soma(-10, 20)}')
+
+# Com o try except evitamos o encerramento do programa, a execução é mantida
+try:
+    print(soma('10', 20))
+except AssertionError as e:
+    print(f'A operação falhou: {e}')
